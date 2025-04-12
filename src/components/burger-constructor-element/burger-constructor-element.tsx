f5a600642b8f6be71a7c16a40e6694ee531a50ea
Eugene Shalnev <mr-voltrein@yandex.ru>
2025-04-12 20:14:07 +0300

    feat: Добавить функционал элемента конструктора

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,25 @@
 import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
+import { useDispatch } from '../../services/store';
+import {
+  removeFromConstructor,
+  reoderConstructor
+} from '../../services/slices/constructor-slice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch = useDispatch();
 
-    const handleMoveUp = () => {};
+    const handleMoveDown = () => {
+      dispatch(reoderConstructor({ from: index, to: index + 1 }));
+    };
 
-    const handleClose = () => {};
+    const handleMoveUp = () => {
+      dispatch(reoderConstructor({ from: index, to: index - 1 }));
+    };
+
+    const handleClose = () => dispatch(removeFromConstructor(index));
 
     return (
       <BurgerConstructorElementUI
