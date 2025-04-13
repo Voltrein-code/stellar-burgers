e06bde744879c6f472eb3fd1fc277e62b3102895
Eugene Shalnev <mr-voltrein@yandex.ru>
2025-04-13 10:54:55 +0300

    feat: Добавить селекторы в order-info

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -2,20 +2,13 @@ import { FC, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useSelector } from '../../services/store';
+import { ingredientsDataSelector, orderInfoSelector } from '@selectors';
+import { useParams } from 'react-router-dom';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
-
-  const ingredients: TIngredient[] = [];
+  const orderData = useSelector(orderInfoSelector(useParams().number || ''));
+  const ingredients = useSelector(ingredientsDataSelector);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
